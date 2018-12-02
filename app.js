df64726aa7281318c91f6937de14a62935d58df0
Jason Paredes <haloos@me.com>
2018-12-01 23:04:14 -0500

    Fetch example

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
-//const sayHello = function() {
-  //console.log('Hello');
-//}
+document.getElementById('button1').addEventListener('click', getText);
 
-//const sayHello = ()  => {
-  //console.log('Hello');
-//}
+document.getElementById('button2').addEventListener('click', getJson);
 
-// One line function does not need braces
-//const sayHello = () => console.log('Hello');
+document.getElementById('button3').addEventListener('click', getExternal);
 
-// One line says returns
-//const sayHello = () => 'Hello';
-
-// Same as above
-//const sayHello = function() {
-  //return 'Hello';
-//}
+// Get local text file data
+function getText() {
+  fetch('test.txt')
+    .then(res => res.text())
+    .then(data => {
+      console.log(data);
+      document.getElementById('output').innerHTML = data;
+    })
+    .catch(err => console.log(err));
+}
 
 
-// Return object
-//const sayHello = () => ({ msg: 'Hello' });
+// Get local json data
+function getJson() {
+  fetch('posts.json')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      let output = '';
+      data.forEach(function(post) {
+        output += `<li>${post.title}</li>`;
+      });
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(err => console.log(err));
+}
 
-// Single param does not need parenthesis
-//const sayHello = name => console.log(`Hello ${name}`);
 
-// Multupile params need parenthesis
-//const sayHello = (firstName, lastName) => console.log(`Hello ${firstName} ${lastName}`);
-
-//sayHello('Jason');
-
-const users = ['Nathan', 'John', 'Dave'];
-
-//const nameLengths = users.map(function(name) {
-  //return name.length;
-//});
-
-// Shorter
-//const nameLengths = users.map((name)  => {
-  //return name.length;
-//});
-
-// Shortest 
-const nameLengths = users.map(name => name.length);
-
-console.log(nameLengths);
+// Get from external API
+function getExternal() {
+  fetch('https://api.github.com/users')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      let output = '';
+      data.forEach(function(user) {
+        output += `<li>${user.login}</li>`;
+      });
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(err => console.log(err));
+}
